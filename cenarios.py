0448d19a4f8d392e2d37d7bad6b8b33f9626877b
Vinicius Hosokawa <vinimoura340@gmail.com>
2026-07-15 11:39:22 -0300

    Finalização projeto cenário 1.0

--- cenarios.py
+++ cenarios.py
@@ -7,7 +7,7 @@ def inicio():
     if comeco == 1:
         escola()
     elif comeco == 2:
-        floresta()  # CORREÇÃO 1: Adicionado para o menu inicial conseguir ir para a floresta!
+        floresta()
     elif comeco == 3:
         casa()
 
@@ -54,7 +54,7 @@ def escola():
         elif escolha == 4:
             print("Saindo da escola e indo para a Floresta...")
             na_escola = False
-            floresta()  # Conectado!
+            floresta()
 
         elif escolha == 5:
             print("Saindo da escola e indo para Casa...")
@@ -99,7 +99,7 @@ def casa():
         elif escolha_casa == 4:
             print("Saindo de casa e indo para a floresta...")
             em_casa = False
-            floresta()  # Conectado!
+            floresta()
 
         elif escolha_casa == 5:
             print("Arrumando a mochila para voltar à escola...")
@@ -112,7 +112,6 @@ def casa():
             inicio()
 
 
-# CORREÇÃO 2: A def floresta() agora está totalmente alinhada na esquerda!
 def floresta():
     em_floresta = True
 
@@ -143,6 +142,70 @@ def floresta():
                 elif decisao2 == 4:
                     print("Parando de caçar...")
                     cacar = False
+
+        elif escolha_floresta == 2:
+            print("\n--- Hora de Pescar! ---")
+            print("Deseja colocar isca?")
+
+            opcao_isca = int(input("1 - Não, 2 - Sim\n> "))
+
+            if opcao_isca == 2:
+                tem_isca = True
+                print("Você preparou o anzol com uma isca!")
+            else:
+                tem_isca = False
+                print("Você decidiu pescar sem isca (pode ser mais difícil).")
+
+            pescar = True
+            while pescar:
+                decisao3 = int(input("\nO que você quer pescar?\n1 - Tilápia, 2 - Jacaré, 3 - Salmão, 4 - Sair\n> "))
+
+                if decisao3 == 1:
+                    if tem_isca:
+                        print("Graças à isca, você pegou uma Tilápia gigante!")
+                    else:
+                        print("Você pescou uma tilápia pequena.")
+                elif decisao3 == 2:
+                    print("Um jacaré surgiu e quase quebrou sua vara! Melhor não mexer com ele.")
+                elif decisao3 == 3:
+                    if tem_isca:
+                        print("Sensacional! Você pescou um Salmão enorme usando a isca!")
+                    else:
+                        print("Você conseguiu pegar um salmão.")
+                elif decisao3 == 4:
+                    print("Guardando a vara de pescar...")
+                    pescar = False
+
+        elif escolha_floresta == 3:
+            print("\n--- Vamos pegar obter um pouco de material ---")
+            print("Quer afiar o machado?")
+
+            afiar = int(input("1 - Não, 2 - Sim\n> "))
+
+            if afiar == 2:
+                afiado = True
+                print("Você afiou seu machado!")
+            else:
+                afiado = False
+                print("Você decidiu ir sem afiar (pode ser mais difícil).")
+
+            coletar = True
+            while coletar:
+                decisao4 = int(input('\nOque você vai tentar coletar hoje?\n1 - Madeira simples, 2 - Madeira refinada, 3 - sair\n> '))
+
+                if decisao4 == 1:
+                    if afiado:
+                        print("Você pegou bastante madeira em cerca de 1 hora!")
+                    else:
+                        print("Você pegou madeira em 2 horas.")
+                elif decisao4 == 2:
+                    if afiado:
+                        print("Boa, você pegou a madeira refinada sem rachar nenhuma!")
+                    else:
+                        print("Você pegou madeira refinada mas acabou perdendo algumas.")
+                elif decisao4 == 3:
+                    print("Indo descansar...")
+                    coletar = False
 
         elif escolha_floresta == 4:
             print("Saindo da floresta e indo para a Escola...")
